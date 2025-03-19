5d4d4bce60017bdca3e44930945af42981655c32
jothimani <jothimani88531@gmail.com>
2025-03-19 13:36:50 +0530

    day1 by mani

---
+++ frontend-for-admin/src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import style from './Dashboard.module.css';
+
+export default function Dashboard(){
+
+
+    return (
+        <>
+        <div className={style.body}>
+           
+        </div>
+        
+        </>
+    )
+}
---
+++ frontend-for-admin/src/Components/Menu/Menu.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ms from './Menu.module,css';
+
+export default function Menu(){
+    <>
+        <div className="menu">
+            <div className="program">
+                <ul>
+                    <li>
+                        programs
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </>
+}
